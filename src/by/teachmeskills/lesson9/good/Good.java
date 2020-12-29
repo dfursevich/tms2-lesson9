90a522c6a12a6ad973f2f7fb0382e181da2900f1
Dzmitry Fursevich <d.fursevich@severex.io>
2020-12-29 21:18:42 +0300

    good example

--- src/by/teachmeskills/lesson9/good/Good.java
+++ src/by/teachmeskills/lesson9/good/Good.java
@@ -1,12 +1,47 @@
 package by.teachmeskills.lesson9.good;
 
+import java.util.Objects;
+
 /**
  * @author Dzmitry Fursevich
  */
 public class Good {
     private Integer id;
+    private String name;
+    private Integer price;
 
     public Good(Integer id) {
         this.id = id;
+    }
+
+    public Good(Integer id, String name, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Good good = (Good) o;
+        return id.equals(good.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/by/teachmeskills/lesson9/good/GoodRunner.java
+++ src/by/teachmeskills/lesson9/good/GoodRunner.java
@@ -9,11 +9,11 @@ import java.util.List;
 public class GoodRunner {
     public static void main(String[] args) {
         List<Good> goods = new ArrayList<>();
-        goods.add(new Good(1));
+        goods.add(new Good(1, "apple", 10));
+        goods.add(new Good(2, "chair", 100));
+        goods.add(new Good(3, "sugar", 15));
 
-
-        int index = goods.indexOf(new Good(1));
-        Good searchedGood = goods.get(index);
-
+        int index = goods.indexOf(new Good(2));
+        System.out.println("index = " + index);
     }
 }
